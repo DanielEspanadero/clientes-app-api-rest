281d84804c4cb5727b533b2f4ad5dc174c3ac3c9
Daniel Españadero <despanadero@mytic.es>
2022-09-05 16:35:48 +0200

    Get by id request error handling

--- src/main/java/com/irondif/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/irondif/springbootbackendapirest/controllers/ClienteRestController.java
@@ -3,11 +3,15 @@ package com.irondif.springbootbackendapirest.controllers;
 import com.irondif.springbootbackendapirest.models.entity.Cliente;
 import com.irondif.springbootbackendapirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -25,8 +29,25 @@ public class ClienteRestController {
 
     @GetMapping("/clientes/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Cliente show(@PathVariable Long id){
-        return clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id){
+
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            cliente = clienteService.findById(id);
+
+        } catch (DataAccessException e){
+            response.put("mensaje", "Error al realizar la consulta en la base de datos!");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+
+        if(cliente == null){
+            response.put("mensaje", "El cliente con el ID: ".concat(id.toString()).concat(" no existe!"));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
     }
 
     @PostMapping("/clientes")
