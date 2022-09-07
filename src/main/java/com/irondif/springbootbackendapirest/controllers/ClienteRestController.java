e67ada7db19f304b747e79c38aec242fe373bd40
Daniel Españadero <despanadero@mytic.es>
2022-09-07 17:11:59 +0200

    update

--- src/main/java/com/irondif/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/irondif/springbootbackendapirest/controllers/ClienteRestController.java
@@ -5,6 +5,9 @@ import com.irondif.springbootbackendapirest.models.services.IClienteService;
 import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -27,6 +30,13 @@ public class ClienteRestController {
     @ResponseStatus(HttpStatus.OK)
     public List <Cliente> index() {
         return clienteService.findAll();
+    }
+
+    @GetMapping("/clientes/page/{page}")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<Cliente> index(@PathVariable Integer page) {
+        Pageable pageable = PageRequest.of(page, 4);
+        return clienteService.findAll(pageable);
     }
 
     @GetMapping("/clientes/{id}")
--- src/main/java/com/irondif/springbootbackendapirest/models/dao/IClienteDao.java
+++ src/main/java/com/irondif/springbootbackendapirest/models/dao/IClienteDao.java
@@ -1,8 +1,8 @@
 package com.irondif.springbootbackendapirest.models.dao;
 
 import com.irondif.springbootbackendapirest.models.entity.Cliente;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
 
 }
