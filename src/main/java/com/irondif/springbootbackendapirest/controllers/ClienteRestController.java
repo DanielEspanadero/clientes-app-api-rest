7afa891d377b6613d7108f05753a8e4526453bbe
Daniel Españadero <despanadero@mytic.es>
2022-09-13 09:46:57 +0200

    See image in controller added

--- src/main/java/com/irondif/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/irondif/springbootbackendapirest/controllers/ClienteRestController.java
@@ -5,10 +5,13 @@ import com.irondif.springbootbackendapirest.models.entity.Cliente;
 import com.irondif.springbootbackendapirest.models.services.IClienteService;
 import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -19,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -223,6 +227,25 @@ public class ClienteRestController {
         }
 
         return new ResponseEntity<Map <String, Object>>(response, HttpStatus.CREATED);
+    }
 
+    @GetMapping("/uploads/img/{nombreFoto:.+}")
+    public ResponseEntity<Resource> verFoto(@PathVariable String nombreFoto){
+        Path rutaArchivo = Paths.get("uploads").resolve(nombreFoto).toAbsolutePath();
+        Resource recurso = null;
+
+        try {
+            recurso = new UrlResource(rutaArchivo.toUri());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        if(!recurso.exists() && !recurso.isReadable()){
+            throw new RuntimeException("Error, no se puede cargar la imagen: " + nombreFoto);
+        }
+        HttpHeaders cabecera = new HttpHeaders();
+        cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"");
+
+        return new ResponseEntity<Resource>(recurso, cabecera, HttpStatus.OK);
     }
 }
